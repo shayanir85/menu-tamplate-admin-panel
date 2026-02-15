62fd97c589328df5a83b221983e1e85068cfb1ad
shayanir85 <shayaniranpor84@gmail.com>
2026-02-15 13:34:40 +0330

    added files

---
+++ Cafe Menu/Js/script.js
@@ -0,0 +1,18 @@
+    document.getElementById('enterBtn').addEventListener('click', function() {
+      const body = document.body;
+      const card = document.querySelector('.glass-card');
+      
+      // انیمیشن خروج
+      card.style.transition = 'all 0.5s ease-in';
+      card.style.transform = 'scale(0.9) translateY(20px)';
+      card.style.opacity = '0';
+      
+      setTimeout(() => {
+          body.style.transition = 'opacity 0.5s ease';
+          body.style.opacity = '0';
+      }, 300);
+
+      setTimeout(() => {
+        window.location.href = 'home.html'; 
+      }, 800);
+    });
