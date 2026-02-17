3682fad51d56bd109cfe55dc3e1e6f36180ae079
shayanir85 <shayaniranpor84@gmail.com>
2026-02-17 10:58:57 +0330

    deleted fruitcake cros added HandleCros

--- Cafe_Menu/Js/axios.js
+++ Cafe_Menu/Js/axios.js
@@ -1,8 +1,1 @@
 
-            axios.get('http://127.0.0.1:8000/api/products')
-                .then(response => {
-                    console.log(response.data);
-                })
-                .catch(error => {
-                    console.error('خطا در دریافت محصولات:', error);
-                });
